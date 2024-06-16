d585bcf98a938cad23843a4ea37ce5600329b6c0
Luis_Eduardo <undernet1@gmail.com>
2024-06-16 15:03:28 -0400

    Initial commit: Added auth guard and basic imports

---
+++ src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './features/home/home.component';
+import { AuthComponent } from './features/auth/auth.component';
+import { authGuard } from './core/guards/auth.guard';
+
+export const routes: Routes = [
+  { path: '', component: HomeComponent, canActivate: [authGuard] },
+  { path: 'auth', component: AuthComponent },
+];
---
+++ src/app/features/auth/auth.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+@Component({
+  selector: 'app-auth',
+  standalone: true,
+  imports: [MatCardModule],
+  templateUrl: './auth.component.html',
+  styleUrl: './auth.component.css',
+})
+export class AuthComponent {}
