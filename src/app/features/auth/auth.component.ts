154632dbf86e0e534d1572f628146d238449a765
Luis_Eduardo <undernet1@gmail.com>
2024-06-17 09:09:31 -0400

    Added auth form

--- src/app/features/auth/auth.component.ts
+++ src/app/features/auth/auth.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { UserAuth } from '../../shared/interfaces/user-auth.interface';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-auth',
   standalone: true,
-  imports: [MatCardModule],
+  imports: [FormsModule, MatCardModule, MatInputModule, MatButtonModule],
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.css',
 })
-export class AuthComponent {}
+export class AuthComponent {
+  user: UserAuth = {
+    username: '',
+    password: '',
+  };
+
+  constructor() {}
+
+  onSubmit(): void {
+    console.log('Username:', this.user.username);
+    console.log('Password:', this.user.password);
+  }
+}
