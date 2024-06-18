c153c01f5ad8b6adf608ddcb13377a113e80e74c
Luis_Eduardo <undernet1@gmail.com>
2024-06-18 16:56:59 -0400

    Removed unnused libraries

--- src/app/features/auth/auth.component.ts
+++ src/app/features/auth/auth.component.ts
@@ -1,20 +1,10 @@
 import { Component } from '@angular/core';
-import { MatCardModule } from '@angular/material/card';
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
 import { UserAuth } from '../../shared/interfaces/user-auth.interface';
 import { FormsModule } from '@angular/forms';
-import { MatIconModule } from '@angular/material/icon';
 @Component({
   selector: 'app-auth',
   standalone: true,
-  imports: [
-    FormsModule,
-    MatCardModule,
-    MatInputModule,
-    MatButtonModule,
-    MatIconModule,
-  ],
+  imports: [],
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.css',
 })
