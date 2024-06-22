d3a9d9dda5c4ab1b19d05c2988cd11d0f4f85066
Luis_Eduardo <undernet1@gmail.com>
2024-06-22 11:32:45 -0400

    Working on the side navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { SidebarComponent } from './core/components/sidebar/sidebar.component';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,27 @@ import { SidebarComponent } from './core/components/sidebar/sidebar.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {}
+export class AppComponent {
+  private _routerSubscription: Subscription;
+  isSidebarVisible = true;
+
+  constructor(private router: Router) {
+    this._routerSubscription = Subscription.EMPTY;
+  }
+
+  ngOnInit(): void {
+    this.toggleSidebarVisibility();
+  }
+
+  ngOnDestroy(): void {
+    this._routerSubscription.unsubscribe();
+  }
+
+  private toggleSidebarVisibility(): void {
+    this._routerSubscription = this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.isSidebarVisible = !event.url.includes('/auth');
+      }
+    });
+  }
+}
--- src/app/core/components/sidebar/sidebar.component.ts
+++ src/app/core/components/sidebar/sidebar.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { NavigationEnd, Router } from '@angular/router';
-import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-sidebar',
@@ -9,27 +7,4 @@ import { Subscription } from 'rxjs';
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css',
 })
-export class SidebarComponent {
-  private _routerSubscription: Subscription;
-  isSidebarVisible = true;
-
-  constructor(private router: Router) {
-    this._routerSubscription = Subscription.EMPTY;
-  }
-
-  ngOnInit(): void {
-    this.toggleSidebarVisibility();
-  }
-
-  ngOnDestroy(): void {
-    this._routerSubscription.unsubscribe();
-  }
-
-  private toggleSidebarVisibility(): void {
-    this._routerSubscription = this.router.events.subscribe((event) => {
-      if (event instanceof NavigationEnd) {
-        this.isSidebarVisible = !event.url.includes('/auth');
-      }
-    });
-  }
-}
+export class SidebarComponent { }
