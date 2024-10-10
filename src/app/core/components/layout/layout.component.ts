ece94ea08cb917822a754c6d07c44c2acef92b8a
Luis_Eduardo <undernet1@gmail.com>
2024-10-10 10:04:36 -0400

    Updated layout

--- src/app/core/components/layout/layout.component.ts
+++ src/app/core/components/layout/layout.component.ts
@@ -1,27 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { SidebarComponent } from '../sidebar/sidebar.component';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
   imports: [RouterOutlet, SidebarComponent],
   templateUrl: './layout.component.html',
-  styleUrl: './layout.component.css',
+  styleUrl: './layout.component.css'
 })
-export class LayoutComponent implements OnInit {
-  isSidebarVisible = true;
+export class LayoutComponent implements OnInit, OnDestroy {
+  private _routerSub$: Subscription;;
+  private hiddenSidebarRoute = '/auth';
+  isSidebarVisible: boolean;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router) {
+    this._routerSub$ = Subscription.EMPTY;
+    this.isSidebarVisible = this.router.url !== this.hiddenSidebarRoute;
+  }
 
   ngOnInit(): void {
     this.toggleSidebarVisibility();
   }
 
+  ngOnDestroy(): void {
+    this._routerSub$.unsubscribe();
+  }
+
   private toggleSidebarVisibility(): void {
-    this.router.events.subscribe((event) => {
+    this._routerSub$ = this.router.events.subscribe((event) => {
       if (event instanceof NavigationEnd) {
-        this.isSidebarVisible = !event.url.includes('/auth');
+        this.isSidebarVisible = event.url !== this.hiddenSidebarRoute;
       }
     });
   }
